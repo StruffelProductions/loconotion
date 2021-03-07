7e5804e137a0b67be6d54672145c0a0aa7d563a5
StruffelProductions <31403260+StruffelProductions@users.noreply.github.com>
2021-03-07 18:03:29 +0100

    Add my scripts

---
+++ run_CC0TexturesGuide.ps1
@@ -0,0 +1,23 @@
+cd $PSScriptRoot
+pip install -r .\requirements.txt
+#$WebFolder="Z:\docs.cc0textures.com\"
+$Toml = "CC0TexturesGuide.toml"
+
+<#if ($WebFolder | Test-Path) {
+    Write-Host  "$WebFolder present!" -ForegroundColor Green
+}else{
+    Write-Host -ForegroundColor Red "$WebFolder not present!"
+    exit
+}#>
+
+Remove-Item ".\dist\CC0TexturesGuide\*"
+python loconotion $Toml
+<#if($?){
+    if ($WebFolder | Test-Path) {
+        Write-Host  "$WebFolder present!" -ForegroundColor Green
+    }else{
+        Write-Host -ForegroundColor Red "$WebFolder not present!"
+        exit
+    }
+    Copy-item -Force -Recurse -Verbose ".\dist\CC0TexturesDocs\*" -Destination $WebFolder
+}#>
